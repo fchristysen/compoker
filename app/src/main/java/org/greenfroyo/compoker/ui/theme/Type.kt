c66800da34fe05d6a056325e553b3116792b7562
fchristysen <fchristysen@traveloka.com>
2023-05-04 11:22:01 +0700

    add font

--- app/src/main/java/org/greenfroyo/compoker/ui/theme/Type.kt
+++ app/src/main/java/org/greenfroyo/compoker/ui/theme/Type.kt
@@ -2,27 +2,36 @@ package org.greenfroyo.compoker.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.greenfroyo.compoker.R
+
+val fonts = FontFamily(
+    Font(resId = R.font.newrodin_light, weight = FontWeight.Light),
+    Font(resId = R.font.newrodin_medium, weight = FontWeight.Normal),
+    Font(resId = R.font.newrodin_bold, weight = FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
+    /* Other default text styles to override
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp
     )
     */
 )
+
